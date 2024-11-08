0f45ed91b6a7ebe4ece55c5fa0d561bca9658d16
Bivek Kasaju <bkasaju97@gmail.com>
2024-11-08 02:37:54 -0500

    portfolio

--- components/Hero/index.jsx
+++ components/Hero/index.jsx
@@ -20,7 +20,7 @@ const Hero = () => {
         <div className="my-7 flex justify-start items-center gap-4 flex-wrap">
           <button className="bg-teal-500 py-2 px-4 font-bold text-white border-2 border-teal-500 rounded-md ease-in-out duration-150 hover:text-white hover:bg-transparent hover:border-white" title="View Resume">
           <a href="/Bivek'sResume.pdf" download className="flex justify-start items-center gap-1">
-              <span>View Resume</span>
+              <span>Download Resume</span>
               <RiProfileLine />
             </a>
           </button>
